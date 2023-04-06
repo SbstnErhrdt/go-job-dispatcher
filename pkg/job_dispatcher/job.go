f85876d67595a5d1ffa8c99f4e83b88edbec58ae
Seb <git@erhardt.net>
2023-04-06 10:53:44 +0200

    order

--- pkg/job_dispatcher/job.go
+++ pkg/job_dispatcher/job.go
@@ -18,20 +18,20 @@ type Job struct {
 	ClientUID  uuid.UUID `gorm:"index" gorm:"type:varchar(36);" json:"clientUID"`
 	OwnerUID   uuid.UUID `gorm:"index" gorm:"type:varchar(36);" json:"ownerUID"`
 	// Attributes
-	StartedAt         *time.Time             `gorm:"index" json:"startedAt"`                                              // the timestamp when the worker started on this job
-	CompletedAt       *time.Time             `gorm:"index" json:"completedAt"`                                            // the timestamp when the worker finished the job
-	LastHeartBeat     *time.Time             `gorm:"index" json:"lastHeartBeat"`                                          // the last time the worker was working on this job
-	Name              string                 `json:"name" gorm:"type:varchar(100);"`                                      // a short and optional name
-	Priority          uint                   `gorm:"index" json:"priority" gorm:"default:1;"`                             // the priority of the job (1 lowest priority, ... 10 high priority)
-	Attempts          uint                   `gorm:"index" json:"attempts" gorm:"default:0;"`                             // the attempts that have been tried in the past
-	CurrentWorkerUID  *uuid.UUID             `gorm:"index" json:"currentWorkerUID" gorm:"type:varchar(36); default:null"` // the uuid of the the worker bot that is currently working in this job
-	WorkerInstance    string                 `gorm:"index" json:"workerInstance" gorm:"index:worker_instance;"`           // the type of worker that should work on this job
-	Parameters        map[string]string      `json:"parameters" gorm:"-"`                                                 // the search parameters the worker should use (e.g. company names ... )
-	ParametersJSON    datatypes.JSON         `json:"-" gorm:"type:jsonb;"`                                                // json
-	Tasks             []JobTask              `json:"tasks" gorm:"-"`                                                      // the way the worker should interact (e.g. click on that, extract this, store it in this bucket, ...)
-	TasksJSON         datatypes.JSON         `json:"-" gorm:"type:jsonb;"`                                                // json
-	CurrentStatus     map[string]interface{} `json:"currentStatus" gorm:"-"`                                              // the current status of the job
-	CurrentStatusJSON datatypes.JSON         `json:"-" gorm:"type:jsonb;"`                                                // json
+	StartedAt         *time.Time             `gorm:"index" json:"startedAt"`                                        // the timestamp when the worker started on this job
+	CompletedAt       *time.Time             `gorm:"index" json:"completedAt"`                                      // the timestamp when the worker finished the job
+	LastHeartBeat     *time.Time             `gorm:"index" json:"lastHeartBeat"`                                    // the last time the worker was working on this job
+	Name              string                 `json:"name" gorm:"type:varchar(100);"`                                // a short and optional name
+	Priority          uint                   `gorm:"index; default:1;" json:"priority"`                             // the priority of the job (1 lowest priority, ... 10 high priority)
+	Attempts          uint                   `gorm:"index; default:0;" json:"attempts"`                             // the attempts that have been tried in the past
+	CurrentWorkerUID  *uuid.UUID             `gorm:"index; type:varchar(36); default:null" json:"currentWorkerUID"` // the uuid of the the worker bot that is currently working in this job
+	WorkerInstance    string                 `gorm:"index" json:"workerInstance"`                                   // the type of worker that should work on this job
+	Parameters        map[string]string      `gorm:"-" json:"parameters"`                                           // the search parameters the worker should use (e.g. company names ... )
+	ParametersJSON    datatypes.JSON         `gorm:"type:jsonb;" json:"-"`                                          // json
+	Tasks             []JobTask              `gorm:"-" json:"tasks"`                                                // the way the worker should interact (e.g. click on that, extract this, store it in this bucket, ...)
+	TasksJSON         datatypes.JSON         `gorm:"type:jsonb;" json:"-"`                                          // json
+	CurrentStatus     map[string]interface{} `gorm:"-" json:"currentStatus"`                                        // the current status of the job
+	CurrentStatusJSON datatypes.JSON         `gorm:"type:jsonb;" json:"-"`                                          // json
 }
 
 // JobTask is a task the worker should perform
